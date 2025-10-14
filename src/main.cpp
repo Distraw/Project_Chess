8b7929cb295f689c475270616f76d31e0b6631ff
Distraw <dmitroisuk11@gmail.com>
2025-10-15 00:19:12 +0300

    Merged from feature/opengl-setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,46 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout << "Hello world!\n";
+    if (glfwInit() == GLFW_FALSE) {
+        cout << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    cout << "MacOS is detected.\n";
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Hello world!!!", 0, 0);
+    if (window == NULL) {
+        cout << "Failed to create window\n";
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    glClearColor(1, 1, 1, 0);
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
